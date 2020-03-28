be68d10f886f5686496104c79a2f6216fca2c7f5
Craig <craigoda@gmail.com>
2020-03-28 14:39:27 -0700

    16:54 - added button in right navbar

--- lib/Navbar/Navbar.dart
+++ lib/Navbar/Navbar.dart
@@ -20,28 +20,45 @@ class Navbar extends StatelessWidget {
 class DesktopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(children: <Widget>[
-        Text("Oppkey: Managers of Online Communities",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontSize: 30
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 1200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+          Text("Oppkey: Managers of Online Communities",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 30
+            ),
           ),
-        ),
-        Row(children: <Widget>[
-          Text("Home", 
-          style: TextStyle(color:Colors.white),
-          ),
-          Text("Portfolio", 
-          style: TextStyle(color:Colors.white),
-          ),
-          Text("Services", 
-          style: TextStyle(color:Colors.white),
-          ),
+          Row(children: <Widget>[
+            Text("Home", 
+            style: TextStyle(color:Colors.white),
+            ),
+            SizedBox(width: 30,),
 
-        ],)
-      ],
+            Text("Portfolio", 
+            style: TextStyle(color:Colors.white),
+            ),
+            SizedBox(width: 30,),
+            Text("About", 
+            style: TextStyle(color:Colors.white),
+            ),
+            SizedBox(width: 30,),
+            MaterialButton(
+              color: Colors.lightBlue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              onPressed: (){},
+              child: Text("Services",
+              style: TextStyle(color: Colors.white),),
+            )
+
+          ],)
+        ],
+        ),
       ),
     );
   }
