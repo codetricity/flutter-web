1141319df73285da65492aef423bcc7ed9f1140d
Craig <craigoda@gmail.com>
2020-03-28 07:12:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# responsive
+
+
+https://youtu.be/rpkKYQCioW0
+
+using VS Code.
+
+## Flutter web reference
+
+* [Official Flutter for Web Getting Started](https://flutter.dev/docs/get-started/web)
---
+++ lib/Navbar/Navbar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return DesktopNavbar();
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+          return DesktopNavbar();
+        } else {
+          return MobileNavbar();
+        }
+      },
+    );
+  }
+}
