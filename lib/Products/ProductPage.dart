5536fe35c2165a4304b4a9e77401efa1123d93ca
Craig <craigoda@gmail.com>
2020-03-30 17:46:48 -0700

    added product page

---
+++ lib/Products/ProductPage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive/Demo/Demo.dart';
+
+
+class ProductPage extends StatelessWidget {
+
+  List<Widget> pageChildren(double width, BuildContext context) {
+    return <Widget> [
+      Container(
+        width: width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Products and Services",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 40.0,
+              color: Colors.white), ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Text("Workflow manage software for marketing campaigns."
+                "Community and event management services.",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white
+                ),),
+              ),
+              MaterialButton(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Demo()),
+                    );    
+
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                    child: Text("Request Demo",
+                    style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+              ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        child: Image.asset("assets/images/flame_success.png",
+        width: width,),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1000) {
+          return  Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: pageChildren(constraints.biggest.width/2, context),);          
+        } else {
+          return Column(
+            children: pageChildren(constraints.biggest.width, context),
+          );
+        }
+      }
+    );
+  }
+}
