04fb2e9ea9c55d20aa2e5dc3b45351f3ef139048
Craig <craigoda@gmail.com>
2020-04-07 15:37:07 -0700

    changed graphic

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/craig/opt/flutter_linux_v1.7.8+hotfix.4-stable/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/craig/Development/practice/flutter/oppkey-site-development"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/craig/opt/flutter_linux_v1.7.8+hotfix.4-stable/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
